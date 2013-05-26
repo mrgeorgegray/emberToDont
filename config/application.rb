4808f91d93034da09500195cc13c88ac0dfd516a
George Gray <george@ummodesign.com>
2013-05-26 09:21:52 +0100

    fix for heroku push

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module ToDontApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # 'cos heroku dont like compiling this handlebars gem
+    config.assets.initialize_on_precompile = false
   end
 end
